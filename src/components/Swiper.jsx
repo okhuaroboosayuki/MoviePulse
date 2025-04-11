d21711f6887fd7d566390eaa3e5f809c83b74e08
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-11 20:54:55 +0100

    feat: implement SwiperCarousel component with movie data fetching and display

---
+++ src/components/Swiper.jsx
@@ -0,0 +1,112 @@
+import { useEffect, useState } from "react";
+import PlayIcon from "/assets/icons/play_Icon.svg";
+import ImdbIcon from "/assets/icons/imdb_icon.svg";
+import RottenIcon from "/assets/icons/rotten_tomatoes_icon.svg";
+
+import { Pagination, Scrollbar, A11y, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+const truncateDecimals = (number, decimals) => {
+  const factor = Math.pow(10, decimals);
+  return Math.round(number * factor) / factor;
+};
+
+const URL = import.meta.env.VITE_IMAGE_URL;
+
+const screenSizeLarge = window.matchMedia("(min-width: 1024px)").matches;
+
+const SwiperCarousel = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const pagination = {
+    clickable: true,
+    el: ".swiper-pagination",
+    renderBullet: function (index, className) {
+      return `<span class="${className}">${index + 1}</span>`;
+    },
+  };
+
+  console.log(screenSizeLarge);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`http://localhost:8000/results`);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Swiper
+      modules={[Pagination, Scrollbar, A11y, Autoplay]}
+      direction="horizontal"
+      allowTouchMove={!screenSizeLarge ? true : false}
+      loop={true}
+      speed={2500}
+      slidesPerView={1}
+      autoplay={{ delay: 5000, disableOnInteraction: false }}
+      pagination={pagination}
+      effect="fade"
+      fadeEffect={{ crossFade: true }}>
+      {!loading &&
+        data.map((movie) => {
+          const imageURL = `${URL}/${movie.backdrop_path}`;
+
+          return (
+            <SwiperSlide key={movie.id}>
+              <div
+                className="flex flex-col justify-center w-full h-full bg-gray-400 bg-blend-multiply"
+                style={{
+                  backgroundImage: `url(${imageURL})`,
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                }}>
+                <div className="sm:w-[404px] w-full xl:ml-25 lg:ml-15 sm:ml-10 px-10 flex flex-col sm:items-start items-center justify-center gap-6">
+                  <h1 className="text-white text-5xl font-bold text-center sm:text-start">{movie.title || movie.name}</h1>
+
+                  <div className="flex items-center justify-center gap-10">
+                    <p className="flex items-center justify-center gap-2.5 text-white font-normal">
+                      <img src={ImdbIcon} alt="IMDB icon" />
+                      <span>{truncateDecimals(movie.vote_average * 10, 1)} / 100</span>
+                    </p>
+
+                    <p className="flex items-center justify-center gap-2.5 text-white font-normal">
+                      <img src={RottenIcon} alt="Rotten Tomatoes icon" />
+                      <span>{Math.ceil(movie.vote_average * 10)}%</span>
+                    </p>
+                  </div>
+
+                  <p className="text-white font-medium text-center sm:text-start text-balance">{movie.overview}</p>
+
+                  <button className="flex items-center gap-2 bg-[#BE123C] py-3 px-6 text-white rounded-md">
+                    <img src={PlayIcon} alt="Play icon" />
+                    <span className="uppercase font-bold">watch trailer</span>
+                  </button>
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+
+      <div className="swiper-pagination-wrapper">
+        <div className="swiper-pagination"></div>
+      </div>
+    </Swiper>
+  );
+};
+
+export default SwiperCarousel;
