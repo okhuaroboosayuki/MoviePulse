49bd82405a9f7e69f454ef64ce6212128f84ebc8
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-11 20:55:59 +0100

    feat: enhance Header component with responsive layout and SwiperCarousel integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import HomeNav from "./HomeNav";
+import SwiperCarousel from "./Swiper";
 
 const Header = () => {
   return (
-    <header className="w-full flex flex-col items-center px-25">
+    <header className="w-full flex flex-col items-center lg:px-15 xl:px-25 px-8 h-screen relative">
       <HomeNav />
+      <SwiperCarousel />
     </header>
   );
 };
