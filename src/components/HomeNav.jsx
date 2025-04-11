457eb68288cbade0056cca550d0a5c26fbc6b400
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-11 20:55:32 +0100

    feat: enhance HomeNav with mobile menu toggle and overflow control

--- src/components/HomeNav.jsx
+++ src/components/HomeNav.jsx
@@ -1,23 +1,88 @@
+import { useEffect } from "react";
 import SearchInput from "./SearchInput";
 import Logo from "/assets/icons/tv.png";
 
+/**
+ * Sets the overflow property of the document based on the provided boolean value.
+ *
+ * @param {boolean} isOpen - If true, disables scrolling by setting overflow to hidden. If false, restores scrolling by resetting overflow properties.
+ */
+function setDocumentOverFlow(isOpen) {
+  isOpen ? (document.body.style.overflow = "") : (document.body.style.overflow = "hidden");
+}
+
 const HomeNav = () => {
+  const handleMenuToggle = () => {
+    const menuIcon = document.getElementById("mobileMenuIcon");
+    const menu = document.getElementById("mobileMenu");
+    const screenSizeLarge = window.innerWidth <= 640;
+
+    if (menuIcon) {
+      menuIcon.classList.toggle("toggle-menu");
+      menu.classList.toggle("hidden");
+
+      menuIcon.classList.contains("toggle-menu") && screenSizeLarge ? setDocumentOverFlow(false) : setDocumentOverFlow(true);
+    }
+  };
+
+  useEffect(() => {
+    const menuIcon = document.getElementById("mobileMenuIcon");
+
+    window.addEventListener("resize", () => {
+      if (menuIcon.classList.contains("toggle-menu")) handleMenuToggle();
+    });
+  }, []);
+
   return (
-    <div className="flex items-center justify-between py-5 w-full">
-      <div className="flex items-center justify-center gap-6">
+    <div className="flex items-center justify-between py-5 w-full z-10">
+      <div className="flex items-center justify-center gap-3 lg:gap-6 text-white text-lg lg:text-2xl font-bold">
         <img src={Logo} alt="MoviePulse's logo" />
         <span>MoviePulse</span>
       </div>
 
       <SearchInput />
 
-      <div className="flex items-center gap-7">
-        <button>sign in</button>
+      <div className="flex items-center gap-7 text-white">
+        <button className="capitalize font-bold lg:block hidden">sign in</button>
 
-        <button id="mobileMenuIcon" class="relative flex flex-col items-start justify-center gap-[6px] z-50" role="button" aria-label="mobile menu button" aria-haspopup="true">
-          <span class="w-7 h-0.5 bg-gray-800 transition-smooth"></span>
-          <span class="w-7 h-0.5 bg-gray-800 transition-smooth"></span>
+        <button
+          id="mobileMenuIcon"
+          className="relative flex flex-col items-start justify-center gap-[6px] z-50 rounded-[100%] bg-[#BE123C] px-2.5 py-3.5 cursor-pointer"
+          role="button"
+          aria-label="mobile menu button"
+          aria-haspopup="true"
+          onClick={handleMenuToggle}>
+          <span className="w-5 h-0.5 bg-white transition-500-in-out"></span>
+          <span className="w-5 h-0.5 bg-white transition-500-in-out"></span>
         </button>
+
+        <div
+          className="hidden bg-white absolute py-10 px-8 z-20 md:top-18 top-0 lg:right-16 xl:right-20 md:right-8 right-0 text-[#333333] animate-fade-in-scale md:w-[25%] lg:w-[18%] xl:w-[15%] w-full md:min-h-0 min-h-screen"
+          id="mobileMenu">
+          <ol className="flex flex-col items-start justify-start gap-6 capitalize mt-20 md:mt-0">
+            <li className="hover:text-[#BE123C] border-b w-full">
+              <a href="">movies</a>
+            </li>
+            <li className="hover:text-[#BE123C] border-b w-full">
+              <a href="">tv series</a>
+            </li>
+            <li className="hover:text-[#BE123C] border-b w-full">
+              <a href="">upcoming</a>
+            </li>
+            <li className="w-full mt-10 md:hidden block">
+              <input
+                type="search"
+                name=""
+                id=""
+                placeholder="What do you want to watch?"
+                className="border h-12 outline-none w-full placeholder:text-[#333333] placeholder:text-[15px] text-[#333333] text-base p-2 rounded-md"
+              />
+            </li>
+            <li className="hover:text-[#BE123C] hover:bg-white border bg-[#BE123C] text-white text-center rounded-md p-3 w-full lg:hidden">
+              <a href="">sign in</a>
+            </li>
+          </ol>
+        </div>
       </div>
     </div>
   );
