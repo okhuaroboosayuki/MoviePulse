7d9a8bb51d9f76ac5d46e42a57488958bc66b93e
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-13 22:20:06 +0100

    fix: updated MovieList component to use trendingMovies and limit displayed movies to 12

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,11 +2,13 @@ import useMovies from "../hooks/useMovies";
 import MovieCard from "./MovieCard";
 
 const MovieList = () => {
-  const { data } = useMovies();
+  const { trendingMovies } = useMovies();
+
+  const slicedData = trendingMovies.slice(0, 12);
 
   return (
-    <ol className="grid grid-cols-4 grid-row-4 gap-20 w-full place-items-start mt-11">
-      {data.map((movie) => (
+    <ol className="grid 2xl:grid-cols-4 2xl:grid-row-4 lg:grid-cols-3 lg:grid-rows-3 md:grid-cols-2 md:grid-rows-2 gap-20 w-full place-items-start mt-11">
+      {slicedData.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
     </ol>
