3e387845253b4cb4fdb695f9bbb045996e941b84
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-13 22:20:49 +0100

    fix: improved HomePage layout for better responsiveness and adjusted heading styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,15 +3,15 @@ import rightArrow from "/assets/icons/right_icon.svg";
 
 const HomePage = () => {
   return (
-    <div className="flex items-center justify-center h-full flex-col w-full">
+    <div className="flex items-center justify-center h-full flex-col w-full relative">
       <Header />
 
       <div className="flex items-center justify-center flex-col w-full mt-20 lg:px-15 xl:px-27 px-8">
         <div className="flex items-center justify-between w-full">
-          <h1 className="font-bold text-4xl capitalize">featured movie</h1>
+          <h1 className="font-bold md:text-4xl sm:text-2xl text-[20px] capitalize">featured movie</h1>
 
           <div className="flex items-center justify-center gap-2">
-            <span className="text-lg text-[#BE123C]">See more</span>
+            <span className="sm:text-lg text-base text-[#BE123C]">See more</span>
             <img src={rightArrow} alt="right arrow icon" />
           </div>
         </div>
