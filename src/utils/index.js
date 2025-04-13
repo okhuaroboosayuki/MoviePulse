7e2087c98ff57ed6f6d17818b9b7879e0be61cc4
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-13 22:17:31 +0100

    feat: added setDocumentOverFlow utility for managing document overflow

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,3 @@
 export { default as truncateDecimals } from "./truncateDecimals";
 export { default as formatDate } from "./formatDate";
+export { default as setDocumentOverFlow } from "./setDocumentOverFlow";
---
+++ src/utils/setDocumentOverFlow.js
@@ -0,0 +1,10 @@
+/**
+ * Sets the overflow property of the document based on the provided boolean value.
+ *
+ * @param {boolean} isOpen - If true, disables scrolling by setting overflow to hidden. If false, restores scrolling by resetting overflow properties.
+ */
+function setDocumentOverFlow(isOpen) {
+  isOpen ? (document.body.style.overflow = "") : (document.body.style.overflow = "hidden");
+}
+
+export default setDocumentOverFlow;
