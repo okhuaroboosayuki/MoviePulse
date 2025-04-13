103ddad2d3f0c8040a24a656d32d9dfad6694fe7
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-13 22:19:27 +0100

    fix: refactored SearchInput component to accept className and textColor props for better customization

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,18 +1,21 @@
+import { forwardRef } from "react";
 import SearchIcon from "/assets/icons/Search.svg";
 
-const SearchInput = () => {
+const SearchInput = forwardRef(({ className, textColor }, ref) => {
   return (
-    <div className="lg:w-[525px] md:w-[60%] w-full hidden md:flex border-2 border-white py-1.5 px-2.5 rounded-[6px] focus-within:border-gray-300">
+    <div className={className}>
       <input
         type="search"
         name=""
         id=""
         placeholder="What do you want to watch?"
-        className="border-none outline-none w-full placeholder:text-white focus:placeholder:text-[15px] sm:placeholder:text-[15px] text-white text-base"
+        className={`border-none outline-none w-full placeholder:${textColor} focus:placeholder:text-[15px] sm:placeholder:text-[15px] ${textColor} text-base`}
+        style={{ textTransform: "lowercase" }}
+        ref={ref}
       />
       <img src={SearchIcon} alt="Search'icon" className="cursor-pointer" />
     </div>
   );
-};
+});
 
 export default SearchInput;
