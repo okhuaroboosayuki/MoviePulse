08dfea54f15ab65b78e75a11b9fdf45f8e66e58a
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-13 01:49:31 +0100

    feat: implement Spinner component with loading animation and styles

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,10 @@
 const Spinner = () => {
   return (
-    <div className="h-full flex items-center justify-center">
-      <div className="">loading...</div>
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="relative h-16 w-16">
+        <div className="absolute inset-0 h-full w-full rounded-full border-6 border-transparent border-t-[#BE123C] animate-spin"></div>
+        <div className="absolute inset-0 h-full w-full rounded-full border-6 border-transparent border-b-[#BE123C] animate-spin-alternate"></div>
+      </div>
     </div>
   );
 };
