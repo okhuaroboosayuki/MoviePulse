3a73fbcb484e1f2a53f617d65f9805c293600408
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-13 01:51:49 +0100

    feat: implemented routing and layout structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Spinner } from "./components";
+import { MoviesProvider } from "./contexts/MoviesContext";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const SingleMoviePage = lazy(() => import("./pages/SingleMoviePage"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <MoviesProvider>
+      <BrowserRouter>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movie" element={<SingleMoviePage />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </MoviesProvider>
+  );
 };
 
 export default App;
