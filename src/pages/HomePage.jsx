7677403472d6f13a84aa68c25c6d808505081a4d
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-13 01:51:34 +0100

    feat: restructured HomePage layout for improved movie display and navigation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,24 @@
 import { Footer, Header, MovieList } from "../components";
+import rightArrow from "/assets/icons/right_icon.svg";
 
 const HomePage = () => {
   return (
-    <div className="flex items-center justify-between h-full flex-col w-full">
+    <div className="flex items-center justify-center h-full flex-col w-full">
       <Header />
-      <MovieList />
+
+      <div className="flex items-center justify-center flex-col w-full mt-20 lg:px-15 xl:px-27 px-8">
+        <div className="flex items-center justify-between w-full">
+          <h1 className="font-bold text-4xl capitalize">featured movie</h1>
+
+          <div className="flex items-center justify-center gap-2">
+            <span className="text-lg text-[#BE123C]">See more</span>
+            <img src={rightArrow} alt="right arrow icon" />
+          </div>
+        </div>
+
+        <MovieList />
+      </div>
+
       <Footer />
     </div>
   );
