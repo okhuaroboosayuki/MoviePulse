3f673dd851320fe6a36fec031fc165579e3b953e
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-14 01:18:30 +0100

    fix: updated HomePage to display sliced trending movies and corrected heading text for consistency

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,28 @@
+import { Link } from "react-router-dom";
 import { Footer, Header, MovieList } from "../components";
+import useMovies from "../hooks/useMovies";
 import rightArrow from "/assets/icons/right_icon.svg";
 
 const HomePage = () => {
+  const { trendingMovies } = useMovies();
+
+  const slicedTrendingMovies = trendingMovies.slice(0, 12);
+
   return (
     <div className="flex items-center justify-center h-full flex-col w-full relative">
       <Header />
 
       <div className="flex items-center justify-center flex-col w-full mt-20 lg:px-15 xl:px-27 px-8">
         <div className="flex items-center justify-between w-full">
-          <h1 className="font-bold md:text-4xl sm:text-2xl text-[20px] capitalize">featured movie</h1>
+          <h1 className="font-bold md:text-4xl sm:text-2xl text-[20px] capitalize">featured movies</h1>
 
-          <div className="flex items-center justify-center gap-2">
+          <Link to={"/featured-movies"} className="flex items-center justify-center gap-2">
             <span className="sm:text-lg text-base text-[#BE123C]">See more</span>
             <img src={rightArrow} alt="right arrow icon" />
-          </div>
+          </Link>
         </div>
 
-        <MovieList />
+        <MovieList movies={slicedTrendingMovies} />
       </div>
 
       <Footer />
