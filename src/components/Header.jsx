c8e872ab1948c718e6a960540b02a6c6139d1069
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-14 01:19:07 +0100

    fix: added textColor and borderColor props to HomeNav for improved styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import SwiperCarousel from "./Swiper";
 const Header = () => {
   return (
     <header className="w-full flex flex-col items-center lg:px-15 xl:px-25 px-8 h-screen relative">
-      <HomeNav />
+      <HomeNav textColor={"text-white"} borderColor={"border-white"} />
       <SwiperCarousel />
     </header>
   );
