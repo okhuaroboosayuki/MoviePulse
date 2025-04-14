7f087f64f3b224647ed3be1349d062deef639212
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-14 19:59:54 +0100

    fix: adjusted header positioning to be absolute for better layout control

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import SwiperCarousel from "./Swiper";
 
 const Header = () => {
   return (
-    <header className="w-full flex flex-col items-center lg:px-15 xl:px-25 px-8 h-screen relative">
+    <header className="absolute top-0 w-full flex flex-col items-center lg:px-15 xl:px-25 px-8 h-screen">
       <HomeNav textColor={"text-white"} borderColor={"border-white"} />
       <SwiperCarousel />
     </header>
