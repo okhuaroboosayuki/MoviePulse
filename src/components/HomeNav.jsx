68f283d02fbf416dd710fced035437b44c626585
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-14 01:18:52 +0100

    fix: refactored HomeNav component to use Link for navigation and improved styling with dynamic text color

--- src/components/HomeNav.jsx
+++ src/components/HomeNav.jsx
@@ -2,8 +2,9 @@ import { useEffect } from "react";
 import SearchInput from "./SearchInput";
 import Logo from "/assets/icons/tv.png";
 import { setDocumentOverFlow } from "../utils";
+import { Link } from "react-router-dom";
 
-const HomeNav = () => {
+const HomeNav = ({ textColor, borderColor }) => {
   const handleMenuToggle = () => {
     const menuIcon = document.getElementById("mobileMenuIcon");
     const menu = document.getElementById("mobileMenu");
@@ -29,14 +30,14 @@ const HomeNav = () => {
 
   return (
     <div className="flex items-center justify-between py-5 w-full z-10">
-      <div className="flex items-center justify-center gap-3 lg:gap-6 text-white text-lg lg:text-2xl font-bold z-50">
+      <Link to={"/"} className="flex items-center justify-center gap-3 lg:gap-6 text-lg lg:text-2xl font-bold z-50">
         <img src={Logo} alt="MoviePulse's logo" />
-        <span>MoviePulse</span>
-      </div>
+        <span className={textColor}>MoviePulse</span>
+      </Link>
 
-      <SearchInput className={"lg:w-[525px] md:w-[60%] w-full hidden md:flex border-2 border-white py-1.5 px-2.5 rounded-[6px] focus-within:border-gray-300"} textColor={"text-white"} />
+      <SearchInput className={`lg:w-[525px] md:w-[60%] w-full hidden md:flex border-2 ${borderColor} py-1.5 px-2.5 rounded-[6px] focus-within:border-gray-300`} textColor={textColor} />
 
-      <div className="flex items-center gap-7 text-white">
+      <div className={`flex items-center gap-7 ${textColor}`}>
         <button className="capitalize font-bold lg:block hidden">sign in</button>
 
         <button
@@ -54,20 +55,28 @@ const HomeNav = () => {
           className="hidden bg-white absolute py-10 px-8 z-20 md:top-18 top-0 lg:right-16 xl:right-20 md:right-8 right-0 text-[#333333] animate-fade-in-scale md:w-[25%] lg:w-[18%] xl:w-[15%] w-full md:min-h-0 min-h-screen"
           id="mobileMenu">
           <ol className="flex flex-col items-start justify-start gap-6 capitalize mt-20 md:mt-0">
-            <li className="hover:text-[#BE123C] border-b w-full">
-              <a href="">movies</a>
+            <li className="border-b w-full">
+              <Link to={"/"} className="hover:text-[#BE123C]">
+                movies
+              </Link>
             </li>
-            <li className="hover:text-[#BE123C] border-b w-full">
-              <a href="">tv series</a>
+            <li className="border-b w-full">
+              <Link to={"/"} className="hover:text-[#BE123C]">
+                tv series
+              </Link>
             </li>
-            <li className="hover:text-[#BE123C] border-b w-full">
-              <a href="">upcoming</a>
+            <li className="border-b w-full">
+              <Link to={"/"} className="hover:text-[#BE123C]">
+                upcoming
+              </Link>
             </li>
             <li className="w-full mt-10 md:hidden block">
               <SearchInput className={"border h-12 w-full text-base p-2 rounded-md"} textColor={"text-[#333333]"} />
             </li>
-            <li className="hover:text-[#BE123C] hover:bg-white border bg-[#BE123C] text-white text-center rounded-md p-3 w-full lg:hidden">
-              <a href="">sign in</a>
+            <li className="w-full lg:hidden flex items-center">
+              <Link to={"/"} className="hover:text-[#BE123C] hover:bg-white border bg-[#BE123C] text-white text-center rounded-md p-3 w-full">
+                sign in
+              </Link>
             </li>
           </ol>
         </div>
