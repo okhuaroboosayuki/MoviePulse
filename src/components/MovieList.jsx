c1d44ba44bcbfb472c13a0fe7ce4589cac0d1454
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-14 01:20:53 +0100

    refactor: removed useMovies hook and update MovieList to accept movies as props

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,14 @@
-import useMovies from "../hooks/useMovies";
+import { memo } from "react";
 import MovieCard from "./MovieCard";
 
-const MovieList = () => {
-  const { trendingMovies } = useMovies();
-
-  const slicedData = trendingMovies.slice(0, 12);
-
+const MovieList = memo(function MovieList({ movies }) {
   return (
     <ol className="grid 2xl:grid-cols-4 2xl:grid-row-4 lg:grid-cols-3 lg:grid-rows-3 md:grid-cols-2 md:grid-rows-2 gap-20 w-full place-items-start mt-11">
-      {slicedData.map((movie) => (
+      {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
     </ol>
   );
-};
+});
 
 export default MovieList;
