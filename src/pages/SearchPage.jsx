d98d910e2e1a231d5e9d671d791b9760038bba73
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-16 20:33:06 +0100

    feat: integrated searchMovies function and updated search results handling in SearchPage

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,18 +1,25 @@
+import { useSearchParams } from "react-router-dom";
 import { Footer, HomeNav, MovieList, Spinner } from "../components";
 import useMovies from "../hooks/useMovies";
+import { useEffect } from "react";
 
 const SearchPage = () => {
-  const { isLoading, searchResults } = useMovies();
+  const { isLoading, searchResults, searchMovies } = useMovies();
 
-  const sortedSearchResults = searchResults.sort((a, b) => b.vote_count - a.vote_count);
+  const [searchQuery] = useSearchParams();
+  const query = searchQuery.get("q");
 
-  const totalNumberOfResults = searchResults.length;
+  useEffect(() => {
+    searchMovies(query);
+  }, [query, searchMovies]);
+
+  const sortedSearchResults = (Array.isArray(searchResults) ? searchResults : []).sort((a, b) => b.vote_average - a.vote_average);
 
   return (
     <div className="flex items-center justify-center h-full flex-col w-full lg:px-15 xl:px-27 sm:px-8 px-3">
       <HomeNav textColor={"text-[#111827]"} borderColor={"border-[#111827]/50"} svgStrokeColor={"#111827"} />
 
-      <div className="mt-13 w-full">{isLoading ? <Spinner /> : <MovieList movies={sortedSearchResults} title="search results:" msgText="no results found" totalNum={totalNumberOfResults} />}</div>
+      <div className="mt-13 w-full">{isLoading ? <Spinner /> : <MovieList movies={sortedSearchResults} title="search results:" msgText="no results found" queryText={query} />}</div>
 
       <Footer />
     </div>
