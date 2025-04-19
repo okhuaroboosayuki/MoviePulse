d7d7b78e7e168721ccdbdfef184fc06936d6dec0
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-19 04:31:58 +0100

    feat: added new icon components for Calendar, Home, Logout, Projector, and Tv

---
+++ src/jsx-icons/ProjectorIcon.jsx
@@ -0,0 +1,47 @@
+const ProjectorIcon = ({ width, height, intensity }) => (
+  <svg
+    fill={"#000000"}
+    id="Layer_1"
+    xmlns:x="&ns_extend;"
+    xmlns:i="&ns_ai;"
+    xmlns:graph="&ns_graphs;"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    width="800px"
+    height="800px"
+    viewBox="0 0 24 24"
+    enableBackground="new 0 0 24 24"
+    xmlSpace="preserve"
+    className={`fill-gray-${intensity}`}
+    style={{ width: width, height: height }}>
+    <metadata>
+      <sfw xmlns="&ns_sfw;">
+        <slices />
+        <sliceSourceBounds width={505} height={984} bottomLeftOrigin="true" x={0} y={-120} />
+      </sfw>
+    </metadata>
+    <g>
+      <g>
+        <g>
+          <path d="M17,22H1c-0.6,0-1-0.4-1-1V9c0-0.6,0.4-1,1-1h16c0.6,0,1,0.4,1,1v2.6l4.7-1.6c0.3-0.1,0.6-0.1,0.9,0.1S24,10.7,24,11v8 c0,0.3-0.2,0.6-0.4,0.8c-0.3,0.2-0.6,0.2-0.9,0.1L18,18.4V21C18,21.6,17.6,22,17,22z M2,20h14v-3c0-0.3,0.2-0.6,0.4-0.8 c0.3-0.2,0.6-0.2,0.9-0.1l4.7,1.6v-5.2l-4.7,1.6c-0.3,0.1-0.6,0.1-0.9-0.1C16.2,13.6,16,13.3,16,13v-3H2V20z" />
+        </g>
+      </g>
+      <g>
+        <g>
+          <path d="M6,10c-2.2,0-4-1.8-4-4s1.8-4,4-4s4,1.8,4,4S8.2,10,6,10z M6,4C4.9,4,4,4.9,4,6s0.9,2,2,2s2-0.9,2-2S7.1,4,6,4z" />
+        </g>
+      </g>
+      <g>
+        <g>
+          <path d="M12,10c-2.2,0-4-1.8-4-4s1.8-4,4-4s4,1.8,4,4S14.2,10,12,10z M12,4c-1.1,0-2,0.9-2,2s0.9,2,2,2s2-0.9,2-2S13.1,4,12,4z" />
+        </g>
+      </g>
+      <g>
+        <g>
+          <path d="M12,16H6c-0.6,0-1-0.4-1-1s0.4-1,1-1h6c0.6,0,1,0.4,1,1S12.6,16,12,16z" />
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+export default ProjectorIcon;
---
+++ src/jsx-icons/index.jsx
@@ -0,0 +1,5 @@
+export { default as HomeIcon } from "./HomeIcon";
+export { default as CalendarIcon } from "./CalendarIcon";
+export { default as LogoutIcon } from "./LogoutIcon";
+export { default as ProjectorIcon } from "./ProjectorIcon";
+export { default as TvIcon } from "./TvIcon";
