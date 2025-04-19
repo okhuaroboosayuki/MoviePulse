1827ab09f8a26f8c48bd830bc67d52f1e865a810
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-04-19 04:32:39 +0100

    feat: added SingleMoviePage and SingleTvSeriesPage components with SideNav integration

---
+++ src/pages/SingleMoviePage.jsx
@@ -0,0 +1,13 @@
+import { SideNav } from "../components";
+
+const SingleMoviePage = () => {
+  return (
+    <section className="flex w-full items-center h-full">
+      <SideNav />
+
+      <section>single movie</section>
+    </section>
+  );
+};
+
+export default SingleMoviePage;
---
+++ src/pages/SingleTvSeriesPage.jsx
@@ -0,0 +1,13 @@
+import { SideNav } from "../components";
+
+const SingleTvSeriesPage = () => {
+  return (
+    <section className="flex w-full items-center h-full">
+      <SideNav />
+
+      <section>single series</section>
+    </section>
+  );
+};
+
+export default SingleTvSeriesPage;
